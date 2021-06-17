578d5add3716f80c5d06f1b2646fd463203d1eee
James Upjohn <jammehcow@jammehcow.co.nz>
2021-06-17 23:41:21 +1200

    Add content to exit page

--- src/page/ExitView.tsx
+++ src/page/ExitView.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { Image } from "@chakra-ui/react";
+import { Container, Image, Text, VStack } from "@chakra-ui/react";
 
 import peepoBye from "/assets/peepoBye.3x.gif";
 
 export const ExitView = () => {
     return (
-        <Image display="inline" src={peepoBye} />
+        <VStack textAlign="center" shouldWrapChildren>
+            <Image mt={30} w={300} src={peepoBye} />
+            <Container>
+                <Text mt={40} fontSize="lg">
+                    Hope you enjoyed this stuff and the little yoshi.
+                    <br />
+                    Thanks to all of the people who contributed in some shape or form to this. Y'all some real ones
+                </Text>
+            </Container>
+            <Text mt={50} fontSize="md">
+                cbtL
+                <br />
+                ~jammeh
+            </Text>
+        </VStack>
     );
 }
