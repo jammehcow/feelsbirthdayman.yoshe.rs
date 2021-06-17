c767667f7be201ba49df0baa31e15d50348de18b
James Upjohn <jammehcow@jammehcow.co.nz>
2021-06-17 22:13:45 +1200

    Add cameo video to present #1

--- src/page/PresentView.tsx
+++ src/page/PresentView.tsx
@@ -1,12 +1,30 @@
 import { Button } from "@chakra-ui/react";
 import React from "react";
 
+import {Present} from "../component";
+
 export interface IPresentsViewProps {
     onAllOpened: () => void;
 }
 
 export const PresentView = ({onAllOpened}: IPresentsViewProps) => {
     return (
-        <Button onClick={onAllOpened}>Click me</Button>
+        <>
+            <Present src="/assets/present_01.png" title="A cameo from Carolina Ravassa" width="20vh">
+                <video src="/assets/cameo.mp4"
+                      style={{"maxHeight": "60vh"}}
+                      controls />
+            </Present>
+            <Present src="/assets/present_02.png" title="Yo" width="20vh">
+                {/* TODO: video */}
+            </Present>
+            <Present src="/assets/present_03.png" title="Yo" width="20vh">
+                {/* TODO: video */}
+            </Present>
+            <Present src="/assets/present_04.png" title="Yo" width="20vh">
+                {/* TODO: video */}
+            </Present>
+            <Button onClick={onAllOpened}>Click me</Button>
+        </>
     );
 }
