89ce29f16ea0a6485f25d88b52b06214039c5d81
James Upjohn <jammehcow@jammehcow.co.nz>
2021-06-18 10:14:21 +1200

    Add fade-ins to following views

--- src/page/ExitView.tsx
+++ src/page/ExitView.tsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { Container, Image, Text, VStack } from "@chakra-ui/react";
+import { Container, Image, ScaleFade, Text, VStack } from "@chakra-ui/react";
 
 import peepoBye from "/assets/peepoBye.3x.gif";
 
 export const ExitView = () => {
     return (
-        <VStack textAlign="center" shouldWrapChildren>
-            <Image mt={30} w={300} src={peepoBye} />
-            <Container>
-                <Text mt={40} fontSize="lg">
-                    Hope you enjoyed this stuff and the little yoshi.
+        <ScaleFade in={true}>
+            <VStack textAlign="center" shouldWrapChildren>
+                <Image mt={30} w={300} src={peepoBye} />
+                <Container>
+                    <Text mt={40} fontSize="lg">
+                        Hope you enjoyed this stuff and the little yoshi.
+                        <br />
+                        Thanks to all of the people who contributed in some shape or form to this. Y'all some real ones
+                    </Text>
+                </Container>
+                <Text mt={50} fontSize="md">
+                    cbtL
                     <br />
-                    Thanks to all of the people who contributed in some shape or form to this. Y'all some real ones
+                    ~jammeh
                 </Text>
-            </Container>
-            <Text mt={50} fontSize="md">
-                cbtL
-                <br />
-                ~jammeh
-            </Text>
-        </VStack>
+            </VStack>
+        </ScaleFade>
     );
 }
